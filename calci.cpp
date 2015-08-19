8d9142d6e2e13e5ad1778f7cf4b084d4d5c88475
mugdhak30 <mugdhak.30@gmail.com>
2015-08-19 14:27:08 +0530

    Updated file calci.cpp

--- calci.cpp
+++ calci.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 
+// C++ program for adding two integers
 int main()
 {
   int a=5,b=6,c;
   c=a+b;
   cout<<"The addition of "<<a<<" and "<<b<<" is "<<c;
+  return 0;
 }
